6ff06cfb78de65f2b04dd68a48d66287453f68c3
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-06-15 11:54:11 -0500

    Gulpfile modified. Added SASS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var nodemon     = require('gulp-nodemon');
+var nodemon;
+var browserSync;
+var gulp  = require('gulp');
+var sass  = require('gulp-sass');
 
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['sass'], function () {
+});
+
+gulp.task('dev', ['browser-sync'], function () {
+  gulp.watch('public/assets/styles/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
@@ -13,9 +18,27 @@ gulp.task('browser-sync', ['nodemon'], function() {
   });
 });
 
-gulp.task('nodemon', function () {
+gulp.task('nodemon', ['sass-dev'], function () {
   nodemon({
     script: 'dev-server.js',
     ignore: [ 'public/', 'node_modules/' ]
   });
+});
+
+gulp.task('sass-dev', function () {
+  nodemon = require('gulp-nodemon');
+  browserSync = require('browser-sync');
+
+  return gulp.src('public/assets/styles/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/assets/styles/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('public/assets/styles/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/assets/styles/css'));
 });
