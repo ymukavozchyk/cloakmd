37bc71e0e694550a3753582ffdf97f70904642bb
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-06-15 14:23:06 -0500

    Added loader

--- public/app/app.js
+++ public/app/app.js
@@ -10,8 +10,9 @@ angular.module('cloakmd', [
   .config(['$showdownProvider', function($showdownProvider){
     $showdownProvider.loadExtension('hideMePlease');
   }])
-  .controller('appCtrl', ['$state',
-    function ($state) {
+  .controller('appCtrl', ['$state', '$scope',
+    function ($state, $scope) {
       console.info('CloakMD is online');
+      $scope.loaded = true;
       $state.go('main');
   }]);
