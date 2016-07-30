e2c436c9a4d26a1069e43e390b93f7ee85c85ed7
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-07-30 09:46:15 -0500

    Extended functionality. UI improvements.

--- public/app/main/main-controller.js
+++ public/app/main/main-controller.js
@@ -10,7 +10,10 @@ angular.module('main')
 
     var sampleNotes = [{
       title: 'Untitled',
-      data: sampleData
+      data: sampleData,
+      publicKey: null,
+      privateKey: null,
+      expirationDate: null
     }];
 
     this.notes = localStorageService.get('notes') || sampleNotes;
@@ -18,14 +21,14 @@ angular.module('main')
     this.note = this.notes[0];
 
     this.aceLoaded = function (editor) {
-
       var session = editor.getSession();
       var renderer = editor.renderer;
 
       editor.setTheme('ace/theme/github');
       editor.setHighlightActiveLine(true);
       editor.setShowPrintMargin(false);
-      editor.setOption("scrollPastEnd", false);
+      editor.setOption('scrollPastEnd', false);
+      editor.setOption('maxLines', 'Infinity');
 
       session.setMode('ace/mode/markdown');
       session.setFoldStyle('markbegin');
@@ -46,8 +49,8 @@ angular.module('main')
       localStorageService.set('notes', vm.notes);
     };
 
-    this.titleBlur = function(){
-      if(vm.note.title == ''){
+    this.titleBlur = function () {
+      if (vm.note.title == '') {
         vm.note.title = 'Untitled';
         vm.notes[vm.index] = vm.note;
         localStorageService.set('notes', vm.notes);
@@ -79,6 +82,14 @@ angular.module('main')
     this.selectNote = function (note) {
       vm.index = vm.notes.indexOf(note);
       vm.note = note;
+    }
+
+    this.publishNote = function () {
+      vm.note.publicKey = "sample_public_key";
+      vm.note.privateKey = "sample_private_key";
+      vm.note.expirationDate = new Date();
+
+      localStorageService.set('notes', vm.notes);
     }
   }
   ]);
